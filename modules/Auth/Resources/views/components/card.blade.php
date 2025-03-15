eba016382e70a4bd992c0320cab6016ce33d21c4
Paul Obunga <paulobunga.one@gmail.com>
2025-03-15 18:02:17 +0300

    Updated Code, Fixed Issue with login page

--- modules/Auth/Resources/views/components/card.blade.php
+++ modules/Auth/Resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="row h-100vh align-aitems-center px-0">
+<div class="row h-100vh align-aitems-center px-0 position-relative">
     <div class="col-lg-6 d-flex align-aitems-center">
         <div class="form-wrapper m-auto">
             <div>
@@ -8,7 +8,10 @@
     </div>
     <div class="col-lg-6 login-bg d-none d-lg-block overflow-hidden text-end py-2"
         style="background-image: url({{ setting('site.auth_banner', admin_asset('img/login-bg.png'), true) }});">
+    </div>
+    <!-- Positioned logo at the split -->
+    <div class="position-absolute" style="left: calc(49% - 100px); top: 20%; transform: translateY(-50%); z-index: 10;">
         <img class="brand_logo" src="{{ setting('site.logo_light', admin_asset('img/logo-light.png'), true) }}"
-            alt="">
+            alt="" style="max-width: 200px; background-color: white; border-radius: 10px; padding: 5px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);">
     </div>
 </div>
